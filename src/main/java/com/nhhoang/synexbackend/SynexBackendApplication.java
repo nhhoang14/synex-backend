ce38dce9332d02cd08f143ead32d773f20073abf
nhhoang14 <nhhoangne@gmail.com>
2026-03-01 16:57:04 +0700

    first commit

---
+++ src/main/java/com/nhhoang/synexbackend/SynexBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nhhoang.synexbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynexBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SynexBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhhoang/synexbackend/SynexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhhoang.synexbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynexBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
