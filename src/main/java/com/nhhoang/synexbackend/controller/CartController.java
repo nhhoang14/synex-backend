1997befacc525a8f0bf18bb0e43612a3f7e94037
nhhoang14 <nhhoangne@gmail.com>
2026-03-24 22:36:10 +0700

    Update package

--- src/main/java/com/nhhoang/synexbackend/controller/CartController.java
+++ src/main/java/com/nhhoang/synexbackend/controller/CartController.java
@@ -2,12 +2,9 @@ package com.nhhoang.synexbackend.controller;
 
 import com.nhhoang.synexbackend.model.Cart;
 import com.nhhoang.synexbackend.model.CartItem;
-import com.nhhoang.synexbackend.model.Product;
 import com.nhhoang.synexbackend.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/cart")
--- src/main/java/com/nhhoang/synexbackend/service/CartService.java
+++ src/main/java/com/nhhoang/synexbackend/service/CartService.java
@@ -9,8 +9,6 @@ import com.nhhoang.synexbackend.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 public class CartService {
