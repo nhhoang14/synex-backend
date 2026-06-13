461e649ba1b5cb805d85c2e5d48c7fb7ce4e835e
nhhoang14 <nhhoangne@gmail.com>
2026-06-14 01:43:13 +0700

    Update user

--- src/main/java/com/nhhoang/synexbackend/service/OrderService.java
+++ src/main/java/com/nhhoang/synexbackend/service/OrderService.java
@@ -14,9 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
--- src/main/java/com/nhhoang/synexbackend/service/UserService.java
+++ src/main/java/com/nhhoang/synexbackend/service/UserService.java
@@ -20,7 +20,6 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-
     @Transactional(readOnly = true)
     public List<UserDTO> getAllUsers() {
         return userRepository.findAll().stream().map(this::mapToUserDTO).toList();
